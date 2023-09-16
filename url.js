940b53fdd905f0a2caf134254fe59ac5cbfc22e4
yeye <145186263+yeye0304@users.noreply.github.com>
2023-09-16 15:11:10 +0800

    Update url.js

--- url.js
+++ url.js
@@ -8,7 +8,6 @@
 
 
 const urls = [
-	'https://hhming.moe',
-	'https://github.com/hms5232',
-	'https://gitlab.com/hms5232'
+	'https://forms.gle/WMAupcb4bWWd4X2E6',
+	'https://forms.gle/4KPQK8ajMHoJo92D8'
 ];
